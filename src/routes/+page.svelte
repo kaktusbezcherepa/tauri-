07ac916a568866e7c75df6cd9210d14beec1ec55
kaktusbezcherepa <eli1starboom@gmail.com>
2024-07-08 18:12:13 +0300

    main-page-edit(video)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,77 +1,66 @@
 <script>
-  import { goto } from '$app/navigation';
+  // import { goto } from '$app/navigation';
 
-  let name = "";
+  // let name = "";
 
-  function greet() {
-    goto('/greeting?name=' + encodeURIComponent(name));
-  }
+  // function greet() {
+  //   goto('/greeting?name=' + encodeURIComponent(name));
+  // }
 </script>
 
-<div class="background">
-  <div class="container">
-  <h1>Welcome to Svelte!</h1>
-
-  <p>Enter a name and click "Greet" to go to the greeting page.</p>
-
-  <form class="row" on:submit|preventDefault={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
+  <div class="back-container">
+    <video src="../assets/background/mainmenu.mp4" autoplay muted loop></video>
+    <div class="container">
+      <button>Play</button>
+      <button>Settings</button>
+      <button>Exit</button>
+    </div>
   </div>
-</div>
 
 <style>
-    .background {
-    height: 100%;
-    width: 100%;
-    background: url("../../assets/background/mainmenu.png");
-  }
-  .container {
-    margin: 0;
-    padding-top: 10vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-  }
-  .row {
-    display: flex;
-    justify-content: center;
-  }
+/* .back-container {
+  position: relative;
+  height: 100vh;
+  z-index: 1;
+}
 
-  input,
+.back-container::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  /* background: url("../assets/background/") center center no-repeat;
+  background-size: cover; */
+  /* filter: blur(3px) contrast(120%);
+} */ 
+
+.container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding-top: 45vh;
+  padding-left: 5vw;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 5vh;
+}
+
+button {
+  width: 7vw;
+  height: 5vh;
+  border: 2px solid black;
+  border-radius: 8px;
+  background-color: transparent;
+  font-size: 20px;
+  color: white;
+  background-color: black;
+}
+
+@media(max-width: 400px) {
   button {
-    border-radius: 8px;
-    border: 1px solid transparent;
-    padding: 0.6em 1.2em;
-    font-size: 1em;
-    font-weight: 500;
-    font-family: inherit;
-    color: #0f0f0f;
-    background-color: #ffffff;
-    transition: border-color 0.25s;
-    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+    width: 60px;
   }
-
-  button {
-    cursor: pointer;
-  }
-
-  button:hover {
-    border-color: #396cd8;
-  }
-  button:active {
-    border-color: #396cd8;
-    background-color: #e8e8e8;
-  }
-
-  input,
-  button {
-    outline: none;
-  }
-
-  #greet-input {
-    margin-right: 5px;
-  }
+}
 </style>
