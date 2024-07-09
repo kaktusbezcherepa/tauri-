3c775b435aacf4a6bcadebce0fdfa1dda43ebe8e
Artem <artval570@gmail.com>
2024-07-09 03:00:49 +0300

    fixBack

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,53 +9,33 @@
 </script>
 
   <div class="back-container">
-    <video src="../assets/background/mainmenu.mp4" autoplay muted loop></video>
+    <div class="main-back">
     <div class="container">
       <button>Play</button>
       <button>Settings</button>
       <button>Exit</button>
     </div>
   </div>
+</div>
 
 <style>
-/* .back-container {
-  position: relative;
-  height: 100vh;
-  z-index: 1;
-}
-
-.back-container::before {
-  content: "";
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  /* background: url("../assets/background/") center center no-repeat;
-  background-size: cover; */
-  /* filter: blur(3px) contrast(120%);
-} */ 
 
 .container {
   position: relative;
   display: flex;
   flex-direction: column;
-  padding-top: 45vh;
-  padding-left: 5vw;
+  padding-top: 20vh;
   justify-content: center;
-  align-items: flex-start;
+  align-items: center;
   gap: 5vh;
 }
 
 button {
-  width: 7vw;
-  height: 5vh;
-  border: 2px solid black;
-  border-radius: 8px;
+  font-size: 60px;
+  color: black;
+  font-family: "Indie Flower", cursive;
   background-color: transparent;
-  font-size: 20px;
-  color: white;
-  background-color: black;
+  border: none;
 }
 
 @media(max-width: 400px) {
@@ -63,4 +43,12 @@ button {
     width: 60px;
   }
 }
+
+.main-back {
+  width: 100vw;
+  height: 100vh;
+  background: url('../assets/background/giphy.gif') no-repeat center;
+  background-size: cover;
+}
+
 </style>
