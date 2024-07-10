22dd069d6842805c998d20d399ad748625da13b0
kaktusbezcherepa <eli1starboom@gmail.com>
2024-07-10 18:48:22 +0300

    sss

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,41 +1,46 @@
 <script>
-    function handleClick() {
-      alert('Нажал на картинку');
+  let card = "/src/assets/background/coloda.png";
+
+  function handleClick() {
+    alert('Нажал на картинку');
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      handleClick();
     }
-  
-    function handleKeyPress(event) {
-      if (event.key === 'Enter') {
-        handleClick();
-      }
-    }
+  }
 </script>
 
-
 <div class="back"></div>
-<div class="colodaclass" on:click={handleClick} on:keypress={handleKeyPress} tabindex="0" role="button"></div>
-
+<button class="image-button" on:click={handleClick} on:keypress={handleKeyPress}>
+  <img class="name" src={card} alt="newtaro">
+</button>
 
 <style>
-    
 .back {
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    background: url('/src/assets/background/scale_1200.png') no-repeat center;
-    background-size: cover;
-    z-index: -1;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: url('/src/assets/background/gameback.jpg') no-repeat center;
+  transform: rotate(180deg);
+  background-size: cover;
+  z-index: -1;
 }
 
-.colodaclass {
-    position: fixed; 
-    left: 85%;
-    top: 40%;
-    width: 20%;
-    height: 20%;
-    background: url('/src/assets/background/newtaro.png') no-repeat; 
-    background-size: contain; 
-    z-index: 0;
-    transform: rotate(90deg);
+.image-button {
+  border: none;
+  background: none;
+  cursor: pointer;
+  padding: 0;
+  margin-left: 80%;
+  margin-top: 15%;
 }
 
+.name {
+  height: auto;
+  width: 350px;
+  z-index: 4;
+  clip-path: inset(5%);
+}
 </style>
